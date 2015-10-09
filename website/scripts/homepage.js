0c4885ba1dada4ad75ff2e2daee6b24d1170fdd0
Thomas Caudron <caudron_1@hotmail.com>
2015-10-09 17:55:48 +0200

    Create the short bio on homepage from js

--- website/scripts/homepage.js
+++ website/scripts/homepage.js
@@ -11,12 +11,39 @@ function NewsArticle(type, date, name, text) {
 	});
 }
 
+function Result(type, text){
+	var self = this;
+
+	self.type = type;
+	self.text = text;
+}
+
+function BioYear(year){
+	var self = this;
+
+	self.year = year;
+	self.results = [
+		new Result("uitleg", "Uitleg over dit jaar kan hier komen met wat tijden en nog vanalle andere dingen!"),
+		new Result("ek", "7<sup>e</sup> 3000m Steeple ETC Heraklion (GRE)"),
+		new Result("bk", "2<sup>e</sup> 3000m Steeple (AC)"),
+		new Result("pk", "1<sup>e</sup> 1500m (senioren)")
+		
+	];
+}
+
 function Homepage() {
-	this.articles = [ 
+	var self = this;
+
+	self.articles = [ 
 		new NewsArticle("twitter", "25 apr. 2015 // 00:11", "@VerhoevenAstrid", "Geen PK voor mij dit jaar, ziektekiemen hebben er anders over beslist. Aan iedereen die deelneemt: veel succes! #PKOVL @ACLebbeke"),
 		new NewsArticle("facebook", "25 apr. 2015 // 00:11", "@VerhoevenAstrid", "Geen PK voor mij dit jaar, ziektekiemen hebben er anders over beslist. Aan iedereen die deelneemt: veel succes! #PKOVL @ACLebbeke"),
 		new NewsArticle("twitter", "25 apr. 2015 // 00:11", "@VerhoevenAstrid", "Geen PK voor mij dit jaar, ziektekiemen hebben er anders over beslist. Aan iedereen die deelneemt: veel succes! #PKOVL @ACLebbeke")
 	];
+
+	self.years = [
+		new BioYear("'15"),
+		new BioYear("'14")
+	]
 }
 
 ko.applyBindings(new Homepage());
