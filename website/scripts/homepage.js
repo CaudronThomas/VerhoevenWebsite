74cf80a2192c4631e1fce77ef3705203716013a5
Thomas Caudron <caudron_1@hotmail.com>
2015-10-18 15:35:56 +0200

    cleanup

--- website/scripts/homepage.js
+++ website/scripts/homepage.js
@@ -11,18 +11,23 @@ function NewsArticle(type, date, name, text) {
 	});
 }
 
-$.getJSON("data/results.json", function(json) {
 	function Homepage() {
 		var self = this;
 
+		self.years = ko.observableArray();
 		self.articles = [ 
 			new NewsArticle("twitter", "25 apr. 2015 // 00:11", "@VerhoevenAstrid", "Geen PK voor mij dit jaar, ziektekiemen hebben er anders over beslist. Aan iedereen die deelneemt: veel succes! #PKOVL @ACLebbeke"),
 			new NewsArticle("facebook", "25 apr. 2015 // 00:11", "@VerhoevenAstrid", "Geen PK voor mij dit jaar, ziektekiemen hebben er anders over beslist. Aan iedereen die deelneemt: veel succes! #PKOVL @ACLebbeke"),
 			new NewsArticle("twitter", "25 apr. 2015 // 00:11", "@VerhoevenAstrid", "Geen PK voor mij dit jaar, ziektekiemen hebben er anders over beslist. Aan iedereen die deelneemt: veel succes! #PKOVL @ACLebbeke")
 		];
 
-		self.years = ko.observableArray(json);
+		function getData(){
+			$.getJSON("data/results.json", function(json) {
+				self.years(json);
+			});
+		}
+
+		getData();
 	}
 
 	ko.applyBindings(new Homepage());
-});
